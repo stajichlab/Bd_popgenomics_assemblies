7ed161f9a0e74ed0b7229e2d1c8a69586d31b37c
Jason Stajich <jasonstajich.phd@gmail.com>
2019-09-23 14:27:19 -0700

    make phylo tree

---
+++ Bd_virus_variation/tree/make_tree.sh
@@ -0,0 +1,9 @@
+#!?usr/bin/bash
+#SBATCH -p short --out make_tree.log
+
+module load IQ-TREE
+
+if [ ! -f virus_haplotypes.haploid.fasaln ]; then
+	ln -s ../virus_haplotypes.haploid.fasaln .
+fi
+iqtree -s virus_haplotypes.haploid.fasaln -nt 1
