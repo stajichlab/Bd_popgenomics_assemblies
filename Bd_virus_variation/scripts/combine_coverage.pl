fa7ebc76fc5a8f6f8252f408d3ff55f4367d08c3
Jason Stajich <jasonstajich.phd@gmail.com>
2019-09-25 11:08:44 -0700

    make the coverage numbers

---
+++ Bd_virus_variation/scripts/combine_coverage.pl
@@ -0,0 +1,32 @@
+#!/usr/bin/env perl
+use strict;
+use warnings;
+
+
+my $genome_cov = "../Asm_Unmapped_BdVir/coverage";
+my $virus_cov  = "coverage";
+
+my %cov;
+my %chroms;
+foreach my $dir ( $genome_cov, $virus_cov) {
+    opendir(my $gcov => $dir) || die "cannot open $dir: $!";
+    for my $file ( readdir($gcov) ) {
+	next unless ( $file =~ /(\S+)\.regions\.bed\.gz$/);
+	my $strain = $1;
+	$strain =~ s/[\(\)]//g;
+	open(my $fh => "gzip -dc \"$dir/$file\" |");
+	while(<$fh>) {
+	    my ($chrom,$start,$end,$coverage) = split;
+	    if ( ! exists $chroms{$chrom} ) {
+		$chroms{$chrom} = abs($end - $start);
+	    }
+	    $cov{$strain}->{$chrom} = $coverage;
+	}
+    }
+}
+my @chroms = sort keys %chroms;
+print join("\t", @chroms), "\n";
+for my $strain ( sort keys %cov) {
+    print join("\t", $strain, map { sprintf("%.2f",$cov{$strain}->{$_} || 0) } 
+	       @chroms),"\n";
+}
