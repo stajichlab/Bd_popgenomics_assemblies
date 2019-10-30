c531e2e88bccecd8858be8108eaf4a57962a9614
Jason Stajich <jasonstajich.phd@gmail.com>
2019-10-30 00:44:38 -0700

    generate proper desc from uniprot hits for records that need curation or updates for gene2product

---
+++ annotate/scripts/uniprot_add_desc.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+import  urllib.request, csv, sys, warnings,re
+import codecs
+urlbase="https://www.uniprot.org/uniprot/?query={}&sort=score&columns=id,entry%20name,reviewed,organism,genes(PREFERRED),genes,protein%20names&format=tab&limit=1"
+if len(sys.argv) < 2:
+    print("No file argument")
+    sys.exit(2)
+
+file = sys.argv[1]
+pat = re.compile(r'^(.+?)\s+\(')
+with open(file,'r') as genes:
+    dat = csv.reader(genes,delimiter="\t")
+    for line in dat:
+        if line[0].startswith("#"):
+            continue
+        url=urlbase.format(line[0])
+#        warnings.warn("url is {}".format(url))
+        with urllib.request.urlopen(url) as uniprot:
+            uniprotdat = csv.reader(codecs.iterdecode(uniprot,'utf-8'),delimiter="\t")
+            header = 0
+            for row in uniprotdat:
+                if header == 0:
+                    header = 1
+                    continue
+                newdesc = row[6]
+                m = pat.match(newdesc)
+                if m:
+                    newdesc = m.group(1)
+
+                print("\t".join([line[0],newdesc]))
