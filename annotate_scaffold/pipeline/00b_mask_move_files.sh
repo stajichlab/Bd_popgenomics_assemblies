43d2451abfe24e130644902f0dc648885a6ac3c6
Jason Stajich <jasonstajich.phd@gmail.com>
2019-12-18 14:16:22 -0800

    funannotate steps

--- annotate_scaffold/pipeline/00b_mask_move_files.sh
+++ annotate_scaffold/pipeline/00b_mask_move_files.sh
@@ -1,1 +1,8 @@
-../../annotate/pipeline/00b_mask_move_files.sh
+#!/bin/bash
+for d in *.mask.*; do 
+	b=$(echo $d | perl -p -e 's/\.mask\.\d+//'); 
+	mv $d/funannotate-mask.log logs/mask.$b.log; 
+	mv $d/repeatmodeler-library.*.fasta repeat_library/$b.repeatmodeler-library.fasta; 
+	rmdir $d
+done
+
--- annotate_scaffold/pipeline/03_update.sh
+++ annotate_scaffold/pipeline/03_update.sh
@@ -1,1 +1,56 @@
-../../annotate/pipeline/03_update.sh
+#!/bin/bash -l
+#SBATCH --nodes=1
+#SBATCH --ntasks=8
+#SBATCH --mem 64gb
+#SBATCH --time=3-00:00:00   
+#SBATCH --output=logs/update.%a.log
+#SBATCH --job-name="fun-update-Bd"
+module unload perl
+module unload miniconda2
+module unload miniconda3
+module load anaconda3
+module load funannotate/development
+module unload perl
+module unload python
+source activate funannotate
+
+export AUGUSTUS_CONFIG_PATH=$(realpath lib/augustus/3.3/config)
+
+CPUS=$SLURM_CPUS_ON_NODE
+
+if [ ! $CPUS ]; then
+ CPUS=2
+fi
+
+INDIR=genomes
+OUTDIR=funannotate
+mkdir -p $OUTDIR
+
+SAMPFILE=samples.tab
+N=${SLURM_ARRAY_TASK_ID}
+
+if [ ! $N ]; then
+    N=$1
+    if [ ! $N ]; then
+        echo "need to provide a number by --array or cmdline"
+        exit
+    fi
+fi
+MAX=`wc -l $SAMPFILE | awk '{print $1}'`
+
+if [ $N -gt $MAX ]; then
+    echo "$N is too big, only $MAX lines in $SAMPFILE"
+    exit
+fi
+species_name=Batrachochytrium_dendrobatidis
+
+sed -n ${N}p $SAMPFILE | while read SAMP
+do
+    name=$(echo "$SAMP" | perl -p -e 's/[\(\)]//g')
+    PREFIX=$(echo "$name" | perl -p -e 's/\-/./g')
+    if [ ! -f $INDIR/$name.masked.fasta ]; then
+	echo "No genome for $INDIR/$name.masked.fasta yet - run 00_mash.sh $N"
+	exit
+    fi
+    funannotate update -i $OUTDIR/$name --cpus $CPUS --sbt lib/Bd_pangenome.sbt
+done
